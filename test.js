2e44f06986077caff1abf2fc061f45daef7b93f9
Reto <rethab@protonmail.ch>
2022-06-03 12:57:39 +0200

    line

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ describe('test', () => {
   const a = { created: new Date() };
   const b = { created: new Date(new Date().getTime() + 100) };
 
-  it('fails', () => {
+  it('which line is the error reported?', () => {
     expect(a).toStrictEqual(b);
   });
 
