a80b7c71d87450e45e46e29c75c3933e90b9c8f0
Reto <rethab@protonmail.ch>
2022-06-03 13:01:44 +0200

    more test data

--- test.js
+++ test.js
@@ -1,7 +1,17 @@
 describe('test', () => {
 
-  const a = { created: new Date() };
-  const b = { created: new Date(new Date().getTime() + 100) };
+  const a = {
+      foo: 'bar',
+      created: new Date(),
+      environment: 'production',
+      test: 12345,
+      id: '12434-124455-114454',
+      owner: 'rethab',
+      repo: 'actions-test-repo',
+  };
+
+  const b = { ...a, created: new Date(new Date().getTime() + 100) };
+
 
   it('which line is the error reported?', () => {
     expect(a).toStrictEqual(b);
